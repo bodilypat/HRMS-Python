0c8bc5a490b8281796a5792d05cab4ddd1b7d7ca
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-09-07 08:47:46 -0700

    Create auther.php

---
+++ app/Models/auther.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use illuminate\Database\Eloquent\Model;
+
+class auther extends Model
+{
+    use HasFactory;
+
+    protected $guarded=[];
+}
