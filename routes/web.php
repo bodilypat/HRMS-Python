089bd569cec1ff6ead151aee5310e91f8318dac8
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-16 08:15:40 -0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\ArtworkController;
 use App\Http\Controllers\ExhibitionArtworkController;
+use App\Http\Controllers\BuyerController;
+use App\Http\Controllers\SaleController;
 
 Route::prefix('exhibitions/{exhibitionId')->group(function(){
     Route::get('artworks',[ExhibitionArtworkController::class,'index'])->name('exhibitions.artworks.index');
@@ -9,4 +13,7 @@ Route::prefix('exhibitions/{exhibitionId')->group(function(){
     Route::delete('artworks/{artworksId', [ExhibinationArtworkController::class,'destroy'])->name('exhibinations.artworks.destroy');
 });
 
+Route::resource('artists', ArtistController::class);
+Route::resource('artworks', ArtworkController::class);
+Route::resource('sales', SaleController::class);
 Route::resource('buyers', BuyerController::class);
