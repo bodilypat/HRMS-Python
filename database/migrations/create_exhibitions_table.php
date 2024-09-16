967ba030f451db388dc1f20560fe29559be4e3a7
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-16 09:27:10 -0700

    Add files via upload

--- database/migrations/create_exhibitions_table.php
+++ database/migrations/create_exhibitions_table.php
@@ -6,19 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateExhibitionsTable extends Migration
 {
+
+    /* Run the migrations. */
     public function up()
     {
         Schema::create('exhibitions', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('name');
             $table->text('description')->nullable();
             $table->date('start_date');
-            $table->date('end_date');
+            $table->date('end_date');        
             $table->string('location');
-            $table->timestamps();
+            $table->timestamps(); // Created a and updated at timestamps
         });
     }
 
+    /* Reverse the migrations */
     public function down()
     {
         Schema::dropIfExists('exhibitions');
