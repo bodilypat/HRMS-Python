63fb62a806cbf717786e435488ba0a42051d04f0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-16 06:34:46 -0700

    Add files via upload

--- resources/views/artist/create.blade.php
+++ resources/views/artist/create.blade.php
@@ -12,7 +12,27 @@
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
             </div>
-
+            <div class="form-group">
+                <label for="biograpy">Biography</label>
+                <textarea name="biography" id="biography" class="form-control">{{ old('biography') }}</textarea>
+                @error('biography')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="Nationality">Nationality</label>
+                <textarea name="nationality" id="nationality" class="form-control">{{ old('nationality') }}</textarea>
+                @error('nationality')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="birth_date">Bio</label>
+                <textarea name="birth_date" id="birth_date" class="form-control">{{ old('birth_date') }}</textarea>
+                @error('birth_date')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
             <div class="form-group">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}">
@@ -20,15 +40,6 @@
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
             </div>
-
-            <div class="form-group">
-                <label for="bio">Bio</label>
-                <textarea name="bio" id="bio" class="form-control">{{ old('bio') }}</textarea>
-                @error('bio')
-                    <div class="text-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
             <button type="submit" class="btn btn-primary">Save Artist</button>
         </form>
     </div>
