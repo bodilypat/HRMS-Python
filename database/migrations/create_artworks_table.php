602fdadb445772cb7c007617f7e8f453bec20ca4
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-16 07:00:30 -0700

    Add files via upload

--- database/migrations/create_artworks_table.php
+++ database/migrations/create_artworks_table.php
@@ -6,20 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateArtworksTable extends Migration
 {
+
+    /* Run the migrations. */
     public function up()
     {
         Schema::create('artworks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('description')->nullable();
-            $table->string('medium');
+            $table->unsignedBigInteger('artist_id'); //Foreing key for artis
             $table->decimal('price', 10, 2)->nullable(); // Price of the artwork
-            $table->string('image')->nullable(); // Path to the image file
-            $table->foreignId('artist_id')->constrained()->onDelete('cascade'); // Foreign key to artists table
+            $table->integer('created_date')->nullable();
+            $table->string('image_path')->nullable(); // Path to the image file
+            $table->enum('available','sold','on loan')->default('avaliable');
             $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('artist_id')->reference('id')->('artist')->onDelete('cascade');
         });
     }
 
+    /* Reverse the migration. */
     public function down()
     {
         Schema::dropIfExists('artworks');
