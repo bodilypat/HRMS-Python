c62f67cce8f20f0d4df5dc7728390b0be9e0c9c2
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-14 16:57:55 -0700

    Create __init__.py

---
+++ backend/app/__init__.py
@@ -0,0 +1,27 @@
+# Backend/app/__init__.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLALchemy
+from flask_migrate import Migrate 
+from dotenv import load_dotenv
+import os
+
+db = SQLALchemy()
+migrate = Migrate()
+
+def create_app():
+	 load_dotenv()
+	 
+	 app = Flask(__name__)
+	 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+	 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	 app.config['SECRETE_KEY'] = os.getenv('SECRETE_KEY', 'default_key')
+	 
+	 db.init_app(app)
+	 migrate.init_app(app, db)
+	 
+	 # Register Blueprints
+	 from app.routes.employee_routes import employee_bp
+	 app.register_blueprint(employee_bp, url_prefix='api/employees')
+	 
+	 return app
