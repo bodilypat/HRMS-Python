357e59ca793c507af65817cb715e2d367170f627
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-15 10:12:55 -0700

    Add files via upload

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -3,19 +3,19 @@
 from .employee_routes import employee_bp
 from .department_routes import department_bp
 from .role_routes import role_bp
-from .attendance_routes import attendance.bp
+from .attendance_routes import attendance_bp
 from .leave_routes import leave_bp
 from .payroll_routes import payroll_bp
-from .training_rotues import training_bp
+from .training_routes import training_bp
 from .permission_routes import permission_bp
 
 blueprints = [
-	(employee_bp, '/api/empployees'),
+	(employee_bp, '/api/employees'),
 	(department_bp, '/api/departments'),
 	(role_bp,'/api/roles'),
 	(attendance_bp,'/api/attendance'),
 	(leave_bp,'/api/leaves'),
-	(payroll_bp,'api/payroll'),
+	(payroll_bp,'/api/payroll'),
 	(training_bp,'/api/training'),
 	(permission_bp,'/api/permissions')
 ]
