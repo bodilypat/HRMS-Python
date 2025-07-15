9eac116853408509500c2cf8aea4231142ef2f08
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-15 14:21:34 -0700

    Create routes.py

---
+++ backend/app/routes/employee/routes.py
@@ -0,0 +1,48 @@
+# Backend/app/routes/employee/routes.py
+
+from flask import Blueprint, request, jsonify
+from app.models import db, Employee
+
+employee_bp = Blueprint('employee', __name__)
+
+# Get all employees  
+@employee_bp.route('/', methods=['GET'])
+	def get_all_employees():
+	employees = Employee.query.all()
+	result = [
+		{
+			"id": e.employee_id,
+			"name": f"{e.first_name} {e.last_name}",
+			'email': e.email
+		} for e in employees
+	]
+	return jsonify(result), 200 
+	
+# POST: Create a new employee
+@employee_bp.route('/', methods=['POST'])
+	def create_employee():
+		data = request.json()
+        try:
+            new_app = Employee(
+                first_name=data.get("first_name"),
+                last_name=data.get("last_name"),
+                email=data.get("email"),
+                phone=data.get("phone"),
+                date_of_birth=data.get("date_of_birth"),
+                hire_date=data.get("hire_date")
+                department_id=data.get("department_id")
+                position_id=data.get("position_id")
+                salary=data.get("salary")
+                status=data.get("status")
+            )
+            de.session.add(new_emp)
+            db.session.commit()
+            
+            return jsonify({
+                "message":"Employee created successfully", 
+                "id": new_emp.employee.id
+            }), 201
+      except Exception as e:
+            db.session.rollback()
+            return jsonify({"error": str(e)}), 400
+	
