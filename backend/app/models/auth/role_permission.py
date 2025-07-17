d402ed22feba2950299e3ce58a1c37aea6ed46b7
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-17 05:48:39 -0700

    Update role_permission.py

--- backend/app/models/auth/role_permission.py
+++ backend/app/models/auth/role_permission.py
@@ -17,21 +17,4 @@ class RolePermission(db.Model):
     
     def __repr__(self):
         return f"<RolePermission role_id={self.role_id}, permission_id={self.permission_id}>"
-
-from . import db
-
-class RolePermission(db.Model):
-	__tablename__ = 'role_permissions'
 	
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	role_id = db.Column(db.Integer, db.ForeignKey('roles.role_id'), nullable=False)
-	permission_id = db.Column(db.Integer, db.ForeignKey('permissions.permission_id'), nullable=False)
-	
-	# Relationshops 
-	role = db.relationship('Role', backref='role_permissions', lazy=True)
-	permission = db.relationship('Permission', backref='role_permissions', lazy=True)
-	
-	def __ref__(self):
-		return f"<RolePermission role_id={self.role_id} permission_id={self.permission_id}>"
-		
-		
