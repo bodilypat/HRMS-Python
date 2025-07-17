68fc01fdc9f61f2a41772265eaa31fab0d27dea0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-17 04:42:32 -0700

    Add files via upload

---
+++ backend/app/models/core/department.py
@@ -0,0 +1,19 @@
+# Backend/app/models/core/department.py
+from app import db
+
+class Depertment(db.Model):
+	__tablename__ = 'departments'
+	
+	depart_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(100), nullable=False, unique=True)
+	
+	# Optional FK to manager 
+	manager_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'), nullable=True)
+	
+	# Relationshiips 
+	manager = db.relationship('Empolyee', foreign_keys=[manager_id], backref='managed_departments' lazy=True)
+	employees = db.relationship('Employee', backref='department', lazy=True)
+	
+	def __repr__(self):
+		return f"<Department {self.name}>"
+		
