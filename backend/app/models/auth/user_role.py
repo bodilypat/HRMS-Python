edad0d4e18fda6823d2c43a8c8da36cb980cf158
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-17 06:04:47 -0700

    Add files via upload

--- backend/app/models/auth/user_role.py
+++ backend/app/models/auth/user_role.py
@@ -1,18 +1,22 @@
 # Backend/app/models/auth/user_role.py
 
-from . import db
-
+from app import db
+    
 class UserRole(db.Model):
-	__tablename__ = 'user_roles'
-	
-	user_role_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	employee_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'), nullable=False)
-	role_id = db.Column(db.Integer, db.ForeignKey('roles.role_id), nullable=False)
-	assigned_date = db.Column(db.Date)
-	
-	# Relationships
-	employee = db.relationship('Employee', backref='user_roles', lazy=True)
-	role = db.relationship('Role', backref='user_roles', lazy=True)
-	
-	def __ref__(self):
-		return f"<UserRole employee_id={self.employee_id} role_id={self.role_id}>"
+    """Associates an employee with a specific role in system."""
+    
+    __tablename__ = 'user_roles'
+    
+    user_role_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.role_id'), nullable=False)
+    assigned_date = db.Column(db.Date)
+    
+    # Relationships
+    employee = db.relationship('Employee', backref=db.backref('user_roles', lazy=True)
+    role = db.relationship('Role', backref=db.backref('user_roles',lazy=True))
+    
+    def __repr__(self):
+        return f"<UserRole employee_id={self.employee_id}, role_id={self.role_id}>"
+        
+        
