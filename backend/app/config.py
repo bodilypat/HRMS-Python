c611c1ba3aa22f2993c7122086cbb1b3dd781340
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-17 04:20:37 -0700

    Add files via upload

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,8 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    """Base configuration with default setting."""
+    
+    # General
 	SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
-	SQLALCHEMY_DATABASE_URI == os.getenv("DATABASE_URL", "sqlite://hrms.db")
+    
+    # Database 
+	SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://hrms.db")
 	SQLALCHEMY_TRACK_MODIFICATIONS = False 
 	
 	# Optional: JWT Configuration
@@ -23,18 +28,21 @@ class Config:
 	MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "false").lower() in ["true", "1"]
 	
 class DevelopmentConfig(Config):
+    """Development environment configuration."""
 	DEBUG = True 
 	
 class TestingConfig(Config):
+    """Testing environment configuration."""
 	TESTING = True 
 	SQLALCHEMY_DATABASE_URI = "sqlite://:memory:" # In-memory test DB
 	WTF_CRSF_ENABLED = False 
 	
 class ProductConfig(Config):
+    """Production environment configuration."""
 	DEBUG = False
 	TESTING = False 
 	
-# Configuration map
+# Configuration mapping for app factory use 
 
 config_by_name = {
 	"development": DevelopmentConfig,
