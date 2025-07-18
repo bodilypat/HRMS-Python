767a6aaa4ab2dfa0d1ed5b98aa762b9a94a91b40
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-18 15:48:42 -0700

    Add files via upload

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,52 +1,21 @@
 # Backend/app/config.py
 
-import os
-from dotenv import load_dotenv
+import os 
 
-# load environment veriable from .env file 
-load_dotenv()
-
-class Config:
-    """Base configuration with default setting."""
+class BaseConfig:
+    SECRET_KEY = os.getenv("SECRET_KEY", "defaut_key")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
     
-    # General
-	SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    ENV = "development"
     
-    # Database 
-	SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://hrms.db")
-	SQLALCHEMY_TRACK_MODIFICATIONS = False 
-	
-	# Optional: JWT Configuration
-	JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret")
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    ENV = "production"
     
-    # Optional: Mail Configuration
-	MAIL_SERVER = os.getenv("MAIL_SERVER", "localhost")
-	MAIL_PORT = int(os.getenv("MAIL_PORT", 25))
-	MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-	MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-	MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "false").lower() in ["true", "1"]
-	MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "false").lower() in ["true", "1"]
-	
-class DevelopmentConfig(Config):
-    """Development environment configuration."""
-	DEBUG = True 
-	
-class TestingConfig(Config):
-    """Testing environment configuration."""
-	TESTING = True 
-	SQLALCHEMY_DATABASE_URI = "sqlite://:memory:" # In-memory test DB
-	WTF_CRSF_ENABLED = False 
-	
-class ProductConfig(Config):
-    """Production environment configuration."""
-	DEBUG = False
-	TESTING = False 
-	
-# Configuration mapping for app factory use 
-
-config_by_name = {
-	"development": DevelopmentConfig,
-	"testing": TestingConfig,
-	"production": ProductionConfig
-}
-
+class TestingConfig(BaseConfig):
+    TESTING = True 
+    ENV = "testing"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:" 
