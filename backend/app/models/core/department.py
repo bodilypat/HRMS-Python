57a4b6d4a1843a11597309c5c7636b51c8b1ed53
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-18 16:21:15 -0700

    Add files via upload

--- backend/app/models/core/department.py
+++ backend/app/models/core/department.py
@@ -1,7 +1,11 @@
 # Backend/app/models/core/department.py
+
 from app import db
 
 class Depertment(db.Model):
+    """
+        Represents a department wihtin the organization.
+    """
 	__tablename__ = 'departments'
 	
 	depart_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -10,10 +14,13 @@ class Depertment(db.Model):
 	# Optional FK to manager 
 	manager_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'), nullable=True)
 	
-	# Relationshiips 
-	manager = db.relationship('Empolyee', foreign_keys=[manager_id], backref='managed_departments' lazy=True)
+	# Relationships 
+	manager = db.relationship('Empolyee', foreign_keys=[manager_id], backref='managed_departments', lazy=True)
 	employees = db.relationship('Employee', backref='department', lazy=True)
 	
 	def __repr__(self):
 		return f"<Department {self.name}>"
+        
+    def __str__(self):
+        return self.name
 		
