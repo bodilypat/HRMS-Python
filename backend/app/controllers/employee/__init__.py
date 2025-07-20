051a53cf0c984405e30473bb76b763c213b0b048
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-20 16:12:19 -0700

    Create __init__.py

---
+++ backend/app/controllers/employee/__init__.py
@@ -0,0 +1,9 @@
+# backend/app/controllers/employee/__init__.py
+
+from .employee_controller import EmployeeController
+
+# Define with is exposed when this package is imported 
+__all__ = [
+	"EmployeeController",
+	]
+	
