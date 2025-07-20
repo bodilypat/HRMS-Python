f3b8cc4620b5a0729bf9d8e9060ddd9e5a99a342
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-20 15:57:16 -0700

    Add files via upload

--- backend/app/controllers/auth/__init__.py
+++ backend/app/controllers/auth/__init__.py
@@ -4,6 +4,7 @@ from .auth_controller import AuthController
 from .role_controller import RoleController 
 from .permission_controller import PermissionController 
 
+# Define what gets imported with from auth import
 __all__ = [
 		"AuthController",
 		"RoleController",
