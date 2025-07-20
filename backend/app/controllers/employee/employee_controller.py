b4e8c92d7a33a04d0ac48b0dee972ae54741f3f5
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-20 16:57:38 -0700

    Add files via upload

--- backend/app/controllers/employee/employee_controller.py
+++ backend/app/controllers/employee/employee_controller.py
@@ -49,3 +49,62 @@ class EmployeeController(BaseController):
         
     def create_employee(self, date):
         required_fields = ["first_name", "last_name", "email", "phone", "department_id", "position_id", "salary", "status"]
+        if not all(data.get(field) for field in reequired_fields):
+            return self.error_response("Missing required employee fields", 400)
+            
+        if Employee.query.filter_by(email=data["email"]).first():
+            return self.error_response("Email already exists", 409)
+            
+            new_employee = Employee(
+                first_name=data["first_name"],
+                last_name=data["last_name"],
+                email=data["email"],
+                phone=data["phone"],
+                department_id=data["department_id"],
+                position_id=data['position_id"],
+                salary=data["salary"],
+                status=data["status"],
+                hire_date=data.get("hire_date")
+            )
+            
+            try: 
+                db.session.add(new_employee)
+                db.session.commit()
+                return self.success_response(
+                    {"id": new_employee.employee_id}
+                    message="Employee created",
+                    status_code=201 
+                )
+           except SQLAlchemyError as e:
+            db.session.rollback()
+            return self.error_response("Database error: " + str(e), 500)
+            
+    def update_employee(self, employee_id, data):
+        employee = Employee.query.get(employee_id)
+        if not employee:
+            return self.error_response("Employee not found", 400)
+            
+            for field in["first_name", "last_name", "email", "phone", "department_id", "position_id", "salary", "status", "hire_date"]
+                if field in data:
+                    setattr(employee, field, data[field])
+                    
+                try:
+                    db.session.commit()
+                    return self.success_response(message="Employee updated")
+                except SQLAlchemyError as e:
+                    return self.error_response("Database error:" + str(e), 500)
+                    
+    def delete_employee(self, employee_id):
+        employee = Employee.query.get(employee_id)
+        
+        if not employee:
+            return self.error_response("Employee not found", 404)
+            
+        try:
+            db.session.delete(employee)
+            db.session.commit()
+            return self.success_response(message="Employee deleted")
+            
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            return self.error_response("Database error: " + str(e), 500)
