f7047739e3bf67cb825568d02f27a9c5b63781dc
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-21 06:45:13 -0700

    Add files via upload

--- backend/database/models.py
+++ backend/database/models.py
@@ -28,12 +28,12 @@ class Employee(db.Model):
 	__tablename__='employees'
 	
 	employee_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	first_name = db.Column(db.String(100))
-	last_name = db.Column(db.String(100))
-	email = db.Column(db.String(150), unique=True)
+	first_name = db.Column(db.String(100), nullable=False)
+	last_name = db.Column(db.String(100), nullable=False)
+	email = db.Column(db.String(150), unique=True, nullable=False)
 	phone = db.Column(db.String(20))
 	date_of_birth = db.Column(db.Date)
-	hire_date = db.Column(db.Date)
+	hire_date = db.Column(db.Date, nullable=False)
 	department_id = db.Column(db.Integer, db.ForeignKey('departments.depart_id'))
 	position_id = db.Column(db.Integer, db.ForeignKey('job_positions.position_id'))
 	salary = db.Column(db.Numeric(10, 2))
