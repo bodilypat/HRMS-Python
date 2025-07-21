4d5ec9bc56ed56568ffd6afe6abdb809b575bda2
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-21 11:08:30 -0700

    Create __init__.py

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,13 @@
+# backend/app/services/__init__.py
+
+from .email_service import send_email
+from .auth_service import authenticate_user, generate_token, verify_token
+from .pdf_generator import generate_pdf 
+
+__all__ = [
+	"send_email",
+	"authenticate_user",
+	"generate_token",
+	"verify_token",
+	"generate_pdf",
+	]
