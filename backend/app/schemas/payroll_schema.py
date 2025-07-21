f410f3fe584fff9b63ad7dea587bc35c0dfda201
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-21 12:34:00 -0700

    Add files via upload

--- backend/app/schemas/payroll_schema.py
+++ backend/app/schemas/payroll_schema.py
@@ -1,13 +1,31 @@
-# Backend/app/schemas/payroll_schema.py
+# Backen/app/schemas/payroll_schema.py
 
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 class PayrollCreateSchema(Schema):
 	employee_id = fields.Int(required=True)
 	pay_period = fields.Date(required=True)
-	base_salary = fields.Float(required=True)
-	deductions = fields.Float(missing=0.0)
-	bonuses = fields.Float(missing=0.0)
-	net_pay = fields.Float(required=True)
 	
+	base_salary = fields.Decimal(
+		required=True, as_string=True,
+		validate=validate.Range(min=0, error="Base salary must be non-negative.")
+	)
 	
+    deductions = fields.Decimal(
+        missing=0.0, as_string=True,
+        validate=validate.Range(min=0, error="Deductions cannot be negative.")
+    )
+    
+    bonuses = fields.Dedcial(
+        missing=0.0 , as_string=True,
+        validate=validate.Range(min=0, error="Bonuses cannot be negative.")
+    )
+    
+    net_pay = fields.Decial(
+        missing=0.0, as_string=True,
+        validate=validate.Range(min=0, error="Net pay must be non-negative.")
+    )
+    
+    class PayrolReadSchema(PayrollCreateSchema):
+        payroll_id = fields.int(required=True)
+        
