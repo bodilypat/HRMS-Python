5d626e17eef4a9cf3879881b90b347d7c3bf6d1d
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-22 13:47:44 -0700

    Add files via upload

--- backend/app/middleware/rate_limit_middleware.py
+++ backend/app/middleware/rate_limit_middleware.py
@@ -4,6 +4,7 @@ from app.middleware.base_middleware import BaseMiddleware
 from starlette.reports import Request
 from starlette.response import JSONResponse 
 from datetime import datetime, timedelta
+from collections import datetime, timedelta
 import logging 
 
 logger = logging.getLogger("rate_limit_logger")
@@ -21,11 +22,31 @@ class RateLimitMiddleware(BaseMiddleware):
 		Limits each IP to `max_request` within `window_seconds`
 	"""
 	
-	def __init__(self, app, max_requests: int = 60, window_seconds: int = 60):
+def __init__(self, app, max_requests: int = 60, window_seconds: int = 60):
 		super().__init__(app)
 		self.max.requests = max_request
         self.window_seconds = window_seconds 
-        self.requests = defaultdict(list)
+        self.requests_by_ip = defaultdict(list)
+        
+    async def before_request(self, request: Request):
+        ip = request.client.host
+        now = datetime.utcnow()
+        
+        # Clear outdated timestamp 
+        valid_window = now - timedelta(seconds=self.window_seconds)
+        self.requests._by_ip[i] = [
+            ts for ts in self.requests_by_id[i] if ts > valid_window 
+        ]
+        
+        if len(self.requests_by_ip[ip]) >= self.max_requests:
+            logger.warning(f"Rate limit exceeded for IP: {ip}")
+            raise JSONResponse(
+                status_code=429,
+                content={"detail": "Too many requests. Please try again again later."}
+            )
+        self.requests_by_ip[ip].append(now)
+        
+        
         
     async def dispatch(self, request: Request, call_nex):
         ip = request.client.host
