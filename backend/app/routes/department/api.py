63dfc2037961007b368624df60a4ec6af567817e
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-23 09:52:55 -0700

    Add files via upload

--- backend/app/routes/department/api.py
+++ backend/app/routes/department/api.py
@@ -1,15 +1,16 @@
-# Backend/app/rotues/department/api.py
+# backend/app/rotues/department/api.py
 
 from flask import blueprint, request, jsonify
-from app.models import db, Departemnt
+from app.models import db
+from app.models.core.department import Department 
 
 department_bp = Blueprint('department', __name__)
 
 # GET: All Department
 @department_bp.route('/', methods=['GET'])
-    def get_all_departments():
-        departments = Departemnt.query.all()
-        result = [
+def get_all_departments():
+    departments = Departemnt.query.all()
+    result = [
             {
                 "id": dept.department_id,
                 "name": dept.name,
@@ -21,9 +22,9 @@ department_bp = Blueprint('department', __name__)
          
 # GET: Department by ID 
 @department_bp.route('/<int:department_id>', methods=['GET'])
-    def get_department(department_id):
-        dept = department.query.get_or_404(department_id)
-        return jsonify({
+def get_department(department_id):
+    dept = department.query.get_or_404(department_id)
+    return jsonify({
             "id": dept.department_id,
             "name": dept.name,
             "description": dept.description
@@ -31,7 +32,7 @@ department_bp = Blueprint('department', __name__)
        
 # POST: Create a new department 
 @department_bp.route('/', methods=['POST'])
-     def create_department():
+def create_department():
      data = request.get_json()
      
      if not data or not data.get("name")
@@ -46,16 +47,19 @@ department_bp = Blueprint('department', __name__)
      try:
         db.session.add(new_dept)
         db.session.commit()
-        return jsonify("message": " Department created", "id": new_dept.department_id}), 201
+        return jsonify({
+            "message": " Department created",
+            "id": new_dept.department_id
+        }), 201
      except Exception as e:
         db.session.rollback()
         return jsonify({"error": str(e)}), 500 
         
 # PUT: Update a deparment
 @department_bp.route('</int:department_id', methods=['PUT'])
-    def update_department(department_id):
-        data = request.get_json()
-        dept = Department.query.get_or_404(department_id) 
+def update_department(department_id):
+    data = request.get_json()
+    dept = Department.query.get_or_404(department_id) 
         
         dept.name = data.get("name", dept.name)
         dept.description = data.get("description", dept.description)
@@ -69,13 +73,13 @@ department_bp = Blueprint('department', __name__)
             
 # DELETE: Remove a department 
 @department_bp.route('/<int:department_id', methods=['DELETE'])
-    def delete_department(department_id):
-        dept = Departemnt.query.get_or_404(department_id)
+def delete_department(department_id):
+    dept = Departemnt.query.get_or_404(department_id)
         
-        try:
-            db.session.delete(dept)
-            db.session.commit()
-            return jsonify({"message": "Department deleted"}), 200
-        except Exception as e:
-            db.session.rollback()
-            return jsonify({"error": str(3)}), 500
+    try:
+        db.session.delete(dept)
+        db.session.commit()
+        return jsonify({"message": "Department deleted"}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(3)}), 500
