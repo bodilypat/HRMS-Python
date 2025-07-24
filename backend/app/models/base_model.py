aaf12d217b7fc672ae9afc2ac69640064eb67c33
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-24 09:31:57 -0700

    Add files via upload

--- backend/app/models/base_model.py
+++ backend/app/models/base_model.py
@@ -2,7 +2,9 @@
 
 from datetime import datetime
 from sqlalchemy import Column, DateTime, func 
-from app.models import db
+from sqlalchemy.orm import declarative_base, Session
+
+Base = declarative_base():
 
 class BaseModel(db.Model):
     __abstract__ = True 
@@ -10,22 +12,22 @@ class BaseModel(db.Model):
     created_at = Column(DateTime, default=func.now(), nullable=False)
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
     
-    def save(self, commit=True):
-        """Convenience method to save the model to the dabase."""
+    def save(self, db_session: Session, commit: bool = True):
+        """Save the current instance to the database."""
         db.session.add(self)
         if commit:
             db.session.commit()
         return self 
         
-    def delete(self, commit=True):
-        """Convenience method to delete the model from the database."""
+    def delete(self, db_session: Session, commit : bool = True):
+        """Delete the current instance from the database."""
         db.session.delete(self)
         if commit:
             db.sessiion.commit()
         
-    def to_dict(self):
-        """Optional: Serialize model to directory."""
-        return {c.name, getattr(self, c.name) for c in self.__table__.columns}
+    def to_dict(self) -> dict:
+        """Convert model fields to directory."""
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
         
      def __repr__(self):
         return f"{self.__class__.__name__} id={getattr(self, 'id', None)}>"
