653f83dd10bb3509f5b3886e588dab6264f62180
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-06-22 10:05:28 -0700

    Add Login component with LoginForm import

---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,13 @@
+//src/features/auth/pages/Login.jsx 
+import LoginForm from "../components/LoginForm";
+
+function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <LogingForm />
+        </div>
+    );
+}
+
+export default Login;
