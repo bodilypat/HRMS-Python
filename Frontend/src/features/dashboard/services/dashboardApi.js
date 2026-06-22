eb79326700de3cd87e3a5d989f90bfe9c9336ca0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-06-22 10:18:07 -0700

    Add dashboardApi.js for dashboard data fetching

---
+++ Frontend/src/features/dashboard/services/dashboardApi.js
@@ -0,0 +1,11 @@
+//src/features/dashboard/services/dashboardApi.js 
+import api from "../../../services/api";
+
+export const getDashboardStats = () => 
+    api.get("/dashboard/stats");
+
+export const getRecentBooking = () => 
+    api.get("/dashboard/bookings");
+
+export const getRevenue = () => 
+    api.get("/dashboard/revenue");
