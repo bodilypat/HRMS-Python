d00f302e35f245c0e35443a4e72587d33e20606a
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-06-22 10:23:33 -0700

    Add constants for dashboard card data

---
+++ Frontend/src/features/dashboard/constants/dashboardCard.js
@@ -0,0 +1,22 @@
+//src/features/dashboard/constants/dashboardCard.js 
+export const DASHBOARD_CARDS = [
+    {
+        title: "Total Rooms",
+        key: "totalRooms"
+    },
+
+    {
+        title: "Bookings",
+        key: "totalBookings"
+    },
+
+    {
+        title: "Customers",
+        key:"customers"
+    },
+
+    {
+        title: "Revenue",
+        key: "revenue"
+    }
+];
