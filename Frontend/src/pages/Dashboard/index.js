1e0624ae65274599195fb663a88240830ce4c1a4
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-06-28 12:57:33 -0700

    Add files via upload

---
+++ Frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,8 @@
+//src/pages/Dashboard/index.js 
+export { default } from "./Dashboard";
+
+/* Options named exports */
+export { default as Dashboard } from "./Dashboard";
+export { default as useDashboard } from "./hooks/useDashboard";
+export { default as dashboardService } from "./services/dashboardService";
+export { default as formatCurrency } from  "./utils/formatCurrency";
